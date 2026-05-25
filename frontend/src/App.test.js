a0dde256b52cb1efbd7c470032aa8ad5da2e3634
Divc18 <magicaworld18@gmail.com>
2026-05-25 22:40:08 +0530

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome message', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/welcome to my react app/i);
-  expect(headingElement).toBeInTheDocument();
-});
